2c94fb470aad553640e477bd1ae32e8687bfa817
mmzsit <tj_2021@163.com>
2021-07-09 00:57:17 +0800

    tj测试

--- text1/src/main/java/com/trkj/text1/Text1Application.java
+++ text1/src/main/java/com/trkj/text1/Text1Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Text1Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Text1Application.class, args);
+    public static void main(String[] argss) {
+        SpringApplication.run(Text1Application.class, argss);
     }
 
 }
